2570fe66d585621bafedf07208728f3bf08ccd57
Marlon Clay <luciousmc@icloud.com>
2019-03-26 17:52:54 -0700

    Practice Drills 02

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,16 +1,57 @@
 
-function sumArray(){
-	
+function sumArray(array){
+    var sumArrayCount = 0;
+    for(var i = 0; i < array.length; i++){
+        sumArrayCount += array[i];
+    }
+    return sumArrayCount;
 }
 
-function fitWithinVal(){
+function fitWithinVal(array, stopNumber){
+    var total = 0;
+    var i = 0;
+    var outputArray = [];
 
+    while (total < stopNumber && i < array.length){
+        total += array[i];
+
+        if (total > stopNumber) {
+            total -= array[i];
+        } else {
+            outputArray.push(array[i]);
+        }
+        i++;
+        }
+    return outputArray;
 }
 
-function getAllNamesShorterThan(){
+function getAllNamesShorterThan(array, length) {
+    var itemLen = 0;
+    var outputArray = [];
 
+    for (var i = 0;i < array.length; i++){
+        itemLen = array[i].length;
+
+        if (itemLen < length){
+            outputArray.push(array[i]);
+        }
+    }
+    return outputArray;
 }
 
-function makeLabel(){
 
+function makeLabel(personObj) {
+    var info = personObj;
+    var fName = info.givenName;
+    var lName = info.familyName;
+    var nameTitle = info.greeting;
+    var streetNumber = info['home address'].streetNumber;
+    var streetName = info['home address'].streetName;
+    var addrCity = info['home address'].city;
+    var addrState = info['home address'].state;
+    var addrZip = info['home address'].zip;
+
+    var output = nameTitle + ' ' + fName + ' ' + lName + '\n' + streetNumber + ' ' + streetName + '\n' + addrCity + ', ' + addrState + ' ' + addrZip;
+
+    return output;
 }
--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,10 +1,25 @@
 
 
-function countOccurences(){
+function countOccurences(array, word){
+    var wordCount = 0;
 
+    for (var i = 0; i < array.length; i++){
+        if (array[i] === word){
+            wordCount += 1;
+        }
+    }
+    return wordCount;
 }
 
-function wordLengths(){
+function wordLengths(array){
+    var strLen = 0;
+    var arrayOutput = [];
+
+    for (var i = 0; i < array.length; i++){
+        strLen = array[i].length;
+
+
+    }
 
 }
 
