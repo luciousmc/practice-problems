f3321ad9b38794345ed26261a9569a8893ae51fc
Marlon <luciousmc@icloud.com>
2019-10-02 11:55:01 -0700

    fitWithinVal function

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,10 +1,28 @@
 
-function sumArray(){
-	
+function sumArray( array ){
+    const arrLen = array.length;
+    let arrSum = 0;
+    for (let i = 0; i < arrLen; i++){
+        arrSum += array[i];
+    }
+    return arrSum;
 }
 
-function fitWithinVal(){
+function fitWithinVal( array, num ){
+    const arrLen = array.length;
+    const output = [];
+    let outputSum = 0;
 
+    for (let i = 0; i < arrLen; i++){
+        let currentNum = array[i];
+        if(currentNum < num){
+            if(outputSum + currentNum < num){
+                output.push(currentNum)
+                outputSum = sumArray(output);
+            }
+        }
+    }
+    return output;
 }
 
 function getAllNamesShorterThan(){
