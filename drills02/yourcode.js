e3536be84030bb8c4f75e354883d0eaeb381f86b
Marlon <luciousmc@icloud.com>
2019-10-09 11:25:57 -0700

    countOccurrences

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -1,7 +1,15 @@
 
 
-function countOccurences(){
+function countOccurences( wordArray, word){
+    const arrLen = wordArray.length;
+    let count = 0;
 
+    for(let i = 0; i < arrLen; i++){
+        if (wordArray[i] === word){
+            count++;
+        }
+    }
+    return count;
 }
 
 function wordLengths(){
