3501af2e7982417ed8a41a92fd06ea7c7fbbaff9
Marlon <luciousmc@icloud.com>
2019-10-27 18:22:51 -0700

    drills 02 completed

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -12,14 +12,58 @@ function countOccurences( wordArray, word){
     return count;
 }
 
-function wordLengths(){
+function wordLengths( wordArray ){
+    const arrLen = wordArray.length;
+    const output = [];
 
+    for(let i = 0; i < arrLen; i++){
+        output.push(wordArray[i].length);
+    }
+    return output;
 }
 
-function getMinMaxMean(){
+function getMinMaxMean( numArray ){
+    const arrLen = numArray.length;
+    const output = {
+        min: numArray[0],
+        max: numArray[0],
+        mean: numArray[0]
+    };
 
+    for (let i = 1; i < arrLen; i++){
+        let currentNum = numArray[i];
+        output.mean += currentNum;
+
+        if (currentNum > output.max){
+            output.max = currentNum;
+        } else if (currentNum < output.min){
+            output.min = currentNum;
+        }
+    }
+    output.mean = Math.ceil(output.mean / arrLen);
+
+    return output;
 }
 
-function findMode(){
+function findMode( numArray ){
+    const arrLen = numArray.length;
+    let maxCount = 0;
+    let currNum = 0;
+    let currCount = 0;
+    let output = 0;
 
+    for (let numI = 0; numI < arrLen; numI++){
+        currNum = numArray[numI];
+        for (let checkNumI = 0; checkNumI < arrLen; checkNumI++){
+            if (numArray[checkNumI] === currNum){
+                currCount++
+            }
+        }
+        if (currCount > maxCount){
+            maxCount = currCount;
+            currCount = 0;
+            output = currNum;
+        }
+    }
+    return output;
 }
